774949611422665b376a7b7834df5dcb4f198d31
lavanya-deva <51946769+lavanya-deva@users.noreply.github.com>
2019-06-22 00:23:00 -0700

    Create prime no int.py

---
+++ prime no int.py
@@ -0,0 +1,11 @@
+lower=int(input())
+upper=int(input())
+for n in range(lower,upper+1):
+    if(n>1):
+        for i in range(2,n):
+            if(n%i==0):
+               break
+            else:
+                print(n)
+                break
+            
